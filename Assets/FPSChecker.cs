1bb84abc35294dce618287897a01e1eb9dc062de
ramisirote <ramisirote@gmail.com>
2020-11-17 20:26:40 +0200

    Small one

---
+++ Assets/FPSChecker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class FPSChecker : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private float refreshRate = 0.2f;
+    private float deltaTime = 0.0f;
+    private float timer;
+
+    // Update is called once per frame
+    void Update() {
+        if (Time.unscaledTime > timer) {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            text.text = fps + " FPS";
+            timer = Time.unscaledTime + refreshRate;
+        }
+    }
+}
